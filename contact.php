b1c59cd05180a9bebfeaab5056cd944927241122
romadebrian <roma_coll04@yahoo.com>
2021-11-05 22:06:33 +0700

    mail send

--- contact.php
+++ contact.php
@@ -2,7 +2,8 @@
 if ($_POST) {
     $Email = "";
     $Name = "";
-    $Isi_Email = "";
+    $Pesan = "";
+    $Email_Penerima = "roma_coll04@yahoo.com";
 
     if (isset($_POST['frm_email'])) {
         $Email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['frm_email']);
@@ -13,26 +14,14 @@ if ($_POST) {
         $Name = filter_var($_POST['frm_name'], FILTER_SANITIZE_STRING);
     }
 
-    if (isset($_POST['visitor_message'])) {
-        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+    if (isset($_POST['frm_pesan'])) {
+        $Pesan = htmlspecialchars($_POST['frm_pesan']);
     }
 
-    if ($concerned_department == "billing") {
-        $recipient = "billing@domain.com";
-    } else if ($concerned_department == "marketing") {
-        $recipient = "marketing@domain.com";
-    } else if ($concerned_department == "technical support") {
-        $recipient = "tech.support@domain.com";
-    } else {
-        $recipient = "contact@domain.com";
-    }
+    $headers  = "From: " . $Email;
 
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-        . 'Content-type: text/html; charset=utf-8' . "\r\n"
-        . 'From: ' . $visitor_email . "\r\n";
-
-    if (mail($recipient, $email_title, $visitor_message, $headers)) {
-        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    if (mail($Email_Penerima, "Contact Roma Github", $Pesan, $headers)) {
+        echo "<p>Thank you for contacting us, $Email. You will get a reply within 24 hours.</p>";
     } else {
         echo '<p>We are sorry but the email did not go through.</p>';
     }
